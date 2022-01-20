4ef5b7dea789b50144d3d85de6bbe93283a2c7b2
takunagai <tak_na@icloud.com>
2022-01-21 08:02:30 +0900

    メインページの内容を追加

--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
 function App() {
   return (
     <div>
-      <h1>Hello, World!</h1>
+      <header>
+        <h1>Cute Dog Images</h1>
+      </header>
+      <main>
+        <section>
+          <figure>
+            <img
+              src="https://images.dog.ceo/breeds/shiba/shiba-8.jpg"
+              alt="cute dog"
+            />
+          </figure>
+        </section>
+      </main>
+      <footer>
+        <p>Dog images are retrieved from Dog API</p>
+        <p><a href="https://dog.ceo/dog-api/about">Donate to Dog API</a></p>
+      </footer>
     </div>
   );
 }
