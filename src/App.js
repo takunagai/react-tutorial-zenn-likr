e6d9c829a05e0e66fdb7438b8d3ea06fb7629773
takunagai <tak_na@icloud.com>
2022-01-21 08:45:21 +0900

    APIのデータを非同期で取得し終わるまで Loading コンポーネントを表示(urls の値が null になっているとしておく)

--- src/App.js
+++ src/App.js
@@ -22,8 +22,17 @@ function Image (props) {
 	);
 }
 
+function Loading () {
+	return <p>Loading...</p>
+}
+
 function Gallery (props) {
 	const urls = props.urls;
+
+	if (urls === null) {
+		return <Loading />;
+	}
+
 	return (
 		<div className="columns is-vcentered is-multiline">
 			{urls.map((url) => {
@@ -38,20 +47,21 @@ function Gallery (props) {
 }
 
 function Main () {
-	const urls = [
-		"https://images.dog.ceo/breeds/shiba/shiba-11.jpg",
-		"https://images.dog.ceo/breeds/shiba/shiba-12.jpg",
-		"https://images.dog.ceo/breeds/shiba/shiba-14.jpg",
-		"https://images.dog.ceo/breeds/shiba/shiba-17.jpg",
-		"https://images.dog.ceo/breeds/shiba/shiba-2.jpg",
-		"https://images.dog.ceo/breeds/shiba/shiba-3i.jpg",
-		"https://images.dog.ceo/breeds/shiba/shiba-4.jpg",
-		"https://images.dog.ceo/breeds/shiba/shiba-5.jpg",
-		"https://images.dog.ceo/breeds/shiba/shiba-6.jpg",
-		"https://images.dog.ceo/breeds/shiba/shiba-7.jpg",
-		"https://images.dog.ceo/breeds/shiba/shiba-8.jpg",
-		"https://images.dog.ceo/breeds/shiba/shiba-9.jpg"
-	];
+	const urls = null;
+	// const urls = [
+	// 	"https://images.dog.ceo/breeds/shiba/shiba-11.jpg",
+	// 	"https://images.dog.ceo/breeds/shiba/shiba-12.jpg",
+	// 	"https://images.dog.ceo/breeds/shiba/shiba-14.jpg",
+	// 	"https://images.dog.ceo/breeds/shiba/shiba-17.jpg",
+	// 	"https://images.dog.ceo/breeds/shiba/shiba-2.jpg",
+	// 	"https://images.dog.ceo/breeds/shiba/shiba-3i.jpg",
+	// 	"https://images.dog.ceo/breeds/shiba/shiba-4.jpg",
+	// 	"https://images.dog.ceo/breeds/shiba/shiba-5.jpg",
+	// 	"https://images.dog.ceo/breeds/shiba/shiba-6.jpg",
+	// 	"https://images.dog.ceo/breeds/shiba/shiba-7.jpg",
+	// 	"https://images.dog.ceo/breeds/shiba/shiba-8.jpg",
+	// 	"https://images.dog.ceo/breeds/shiba/shiba-9.jpg"
+	// ];
 	return (
 		<main>
 			<section className="section">
