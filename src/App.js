24587cf71f1c807363c959b4dfbb8b72cca32197
takunagai <tak_na@icloud.com>
2022-01-21 08:36:49 +0900

    APIから複数の画像を受け取る前提で繰り返しを実装

--- src/App.js
+++ src/App.js
@@ -15,30 +15,48 @@ function Image (props) {
 		<div className="card">
 			<div className="card-image">
 				<figure className="image">
-					<img src={props.src} alt="cute dog" />
+					<img src={props.src} alt="cute dog"/>
 				</figure>
 			</div>
 		</div>
 	);
 }
 
-function Gallery () {
-	const url = "https://images.dog.ceo/breeds/shiba/shiba-8.jpg";
+function Gallery (props) {
+	const urls = props.urls;
 	return (
 		<div className="columns is-vcentered is-multiline">
-			<div className="column is-3">
-				<Image src={url}/>
-			</div>
+			{urls.map((url) => {
+				return (
+					<div key={url} className="column is-3">
+						<Image src={url}/>
+					</div>
+				);
+			})}
 		</div>
 	);
 }
 
 function Main () {
+	const urls = [
+		"https://images.dog.ceo/breeds/shiba/shiba-11.jpg",
+		"https://images.dog.ceo/breeds/shiba/shiba-12.jpg",
+		"https://images.dog.ceo/breeds/shiba/shiba-14.jpg",
+		"https://images.dog.ceo/breeds/shiba/shiba-17.jpg",
+		"https://images.dog.ceo/breeds/shiba/shiba-2.jpg",
+		"https://images.dog.ceo/breeds/shiba/shiba-3i.jpg",
+		"https://images.dog.ceo/breeds/shiba/shiba-4.jpg",
+		"https://images.dog.ceo/breeds/shiba/shiba-5.jpg",
+		"https://images.dog.ceo/breeds/shiba/shiba-6.jpg",
+		"https://images.dog.ceo/breeds/shiba/shiba-7.jpg",
+		"https://images.dog.ceo/breeds/shiba/shiba-8.jpg",
+		"https://images.dog.ceo/breeds/shiba/shiba-9.jpg"
+	];
 	return (
 		<main>
 			<section className="section">
 				<div className="container">
-					<Gallery/>
+					<Gallery urls={urls}/>
 				</div>
 			</section>
 		</main>
